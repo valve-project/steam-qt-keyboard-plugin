13dec4a6b06564440c2e2db69fa880a5a3c4e9ff
David Edmundson <kde@davidedmundson.co.uk>
2022-10-07 16:20:20 +0100

    enable accessibility bus on startup

--- steamdeckkeyboardlauncher/Config.py
+++ steamdeckkeyboardlauncher/Config.py
@@ -2,6 +2,8 @@ import configparser
 import dbus
 from gi.repository import GLib
 from dbus.mainloop.glib import DBusGMainLoop
+import logging
+_logger = logging.getLogger(__name__)
 
 from steamdeckkeyboardlauncher.utils import XDGDirs, EventSource
 
@@ -16,6 +18,10 @@ class Config(EventSource):
         EventSource.__init__(self, self._event_names)
         self.bus.add_signal_receiver(self._handle_settings_changed, "ConfigChanged", "org.kde.kconfig.notify", None, "/kdesteam")
 
+        # enable the a11y bus
+        self.bus.call_async("org.a11y.Bus", "/org/a11y/bus", "org.freedesktop.DBus.Properties", "Set", "ssv",
+                          ["org.a11y.Status", "IsEnabled", True], None, self._on_enable_a11y_error)
+
     def autoshow_enabled(self):
         try:
             config_location = XDGDirs.find_config_file("kdesteam")
@@ -24,6 +30,9 @@ class Config(EventSource):
             return config["Keyboard"].getboolean("showAutomatically", True)
         except:
             return True
+
+    def _on_enable_a11y_error(self):
+        _logger.warning("Failed to enable the accessibility bus")
 
     def _handle_settings_changed(self, args):
         read_settings()
